69b9e8c52ffb98c4e0bc01b7c1f8386b55c6d5c5
muradhaji <murad.haciyev00@gmail.com>
2022-06-13 14:25:40 +0400

    modal hook additional functions

--- src/Hooks/useModalStatus.js
+++ src/Hooks/useModalStatus.js
@@ -1,10 +1,25 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { find } from 'lodash';
+import { openModal, closeModal, toggleModal } from '../Redux/Slices/ModalSlice';
 
 const useModalStatus = (modalKey) => {
   const { modals = [] } = useSelector((state) => state) || {};
+
+  const dispatch = useDispatch();
+
+  const toggleModalState = () => {
+    dispatch(toggleModal(modalKey));
+  };
+
+  const showModal = () => {
+    dispatch(openModal(modalKey));
+  };
+
+  const hideModal = () => {
+    dispatch(closeModal(modalKey));
+  };
 
   const [visible, setVisible] = useState(false);
 
@@ -14,7 +29,7 @@ const useModalStatus = (modalKey) => {
     // eslint-disable-next-line
   }, [modals]);
 
-  return visible;
+  return { visible, showModal, hideModal, toggleModalState };
 };
 
 useModalStatus.propTypes = {
